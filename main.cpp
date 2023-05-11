c2b915a702dc90e679d0b3978c84f05b7cb7056c
Stanisław <Stanislaw.Darowski101@gmail.com>
2023-05-11 03:00:03 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,266 @@
+﻿#define UTF_IMPLEMENTATION
+#include "utf.hpp"
+#include <iostream>
+#include <conio.h>
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+struct ConInfo {
+	COORD cur, size;
+};
+
+COORD baseCurPos;
+ConInfo getCurPos() {
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(hConsole, &csbi);
+	return { csbi.dwCursorPosition , csbi.dwSize };
+}
+
+void cls() {
+	auto info = getCurPos();
+	DWORD ncw = 0;
+	FillConsoleOutputCharacterA(hConsole, ' ', (info.cur.Y - baseCurPos.Y) * info.size.X + info.cur.X, baseCurPos, &ncw);
+}
+
+void clfw(int n) {
+	DWORD ncw = 0;
+	FillConsoleOutputCharacterA(hConsole, ' ', n, getCurPos().cur, &ncw);
+}
+
+uint8_t codepoint_buffer[4]{ 0 };
+void print_codepoint(char32_t codepoint);
+
+uint8_t bytes_buffer[8]{ 0 };
+void print_bytes();
+
+
+const char *utf_result_str(UTF_RESULT r) {
+	switch(r) {
+		case UTF_OK: return "ok";
+		case UTF_UNEXPECTED_CONTINUATION: return "unexpected continuation byte";
+		case UTF_TOO_FEW_WORDS: return "too few words";
+		case UTF_OVERLONG: return "overlong";
+		case UTF_ILLEGAL_CODEPOINT: return "invalid codepoint";
+		default: return "huh?";
+	}
+}
+
+
+int main() {
+
+	SetConsoleOutputCP(65001);
+
+	baseCurPos = getCurPos().cur;
+	{
+		CONSOLE_CURSOR_INFO ci;
+		GetConsoleCursorInfo(hConsole, &ci);
+		ci.bVisible = false;
+		SetConsoleCursorInfo(hConsole, &ci);
+	}
+
+	int mode = 0;
+	int inp_pos_cp = 0, inp_pos_by = 0;
+	bool inp_first_half = true;
+
+	auto inp_move = [&](bool incr) {
+		int *inp_ptr = mode == 0 ? &inp_pos_cp : &inp_pos_by;
+		if(incr)
+			*inp_ptr < (mode == 0 ? 3 : 7) ? ++(*inp_ptr) : 0;
+		else
+			*inp_ptr > 0 ? --(*inp_ptr) : 0;
+
+		inp_first_half = true;
+	};
+
+	for(;;) {
+
+		SetConsoleCursorPosition(hConsole, baseCurPos);
+
+		char32_t currCodepoint = 0;
+		for(int i = 0; i < 4; i++)
+			currCodepoint |= static_cast<char32_t>(codepoint_buffer[i]) << (3 - i) * 8;
+
+		if(mode == 0)
+			print_codepoint(currCodepoint);
+		else
+			print_bytes();
+
+
+		SetConsoleCursorPosition(hConsole, { baseCurPos.X, (short)(baseCurPos.Y + 20) });
+
+
+		auto print_byte = [&](bool isMode, bool isPos, unsigned byte) {
+			printf("%c%.2X%c", isPos ? (isMode ? '<' : '.') : ' ', byte, isPos && isMode ? '>' : ' ');
+		};
+
+		printf(" %s codepoint: 0x", mode == 0 ? "->" : "  ");
+		for(int i = 0; i < 4; i++) {
+			print_byte(mode == 0, inp_pos_cp == i, static_cast<unsigned>(codepoint_buffer[i]));
+		}
+
+		printf("\n\n");
+
+		printf(" %s bytes: 0x", mode == 1 ? "->" : "  ");
+		for(int i = 0; i < 8; i++) {
+			print_byte(mode == 1, inp_pos_by == i, static_cast<unsigned>(bytes_buffer[i]));
+		}
+
+
+		int c = _getch();
+		if(c == 0 || c == 0xE0)
+			c = c << 8 | _getch();
+		//cls();
+		printf("\n\n%.2X %.2X\n", c >> 8, c & 0xFF);
+
+		uint8_t *edit_byte = mode == 0 ? &codepoint_buffer[inp_pos_cp] : &bytes_buffer[inp_pos_by];
+
+		if((c >= '0' && c <= '9') || ((c & 0x5F) >= 'A' && (c & 0x5F) <= 'F')) {
+			uint8_t inp_byte = 0;
+			if(c >= '0' && c <= '9') inp_byte = c - '0';
+			else inp_byte = (c & 0x5F) - 'A' + 10;
+
+			if(inp_first_half) {
+				*edit_byte = (inp_byte << 4) | (*edit_byte & 0x0F);
+				inp_first_half = false;
+			}
+			else {
+				*edit_byte = (*edit_byte & 0xF0) | inp_byte;
+				inp_move(true);
+			}
+			continue;
+		}
+
+		switch(c) {
+			case 8: //bcksp
+				*edit_byte = 0;
+				inp_first_half = true;
+				break;
+
+			case 13: //enter
+				cls();
+				mode = mode == 0 ? 1 : 0;
+				break;
+
+			case 0x2B:
+			case 0x3D: //plus
+				if(mode != 0) break;
+				currCodepoint++;
+				for(int i = 0; i < 4; i++)
+					codepoint_buffer[i] = static_cast<uint8_t>(currCodepoint >> (3 - i) * 8);
+				break;
+
+			case 0x2D:
+			case 0x5F: //minus
+				if(mode != 0) break;
+				currCodepoint--;
+				for(int i = 0; i < 4; i++)
+					codepoint_buffer[i] = static_cast<uint8_t>(currCodepoint >> (3 - i) * 8);
+				break;
+
+			case 0xE04B: //left
+				inp_move(false);
+				break;
+
+			case 0xE04D: //right
+				inp_move(true);
+				break;
+
+			case 0xE048: //up
+				if(*edit_byte != 0xFF)
+					(*edit_byte)++;
+				break;
+
+			case 0xE050: //down
+				if(*edit_byte != 0)
+					(*edit_byte)--;
+				break;
+
+			case 0xE053: //del
+				*edit_byte = 0;
+				inp_move(true);
+				break;
+
+			case 0xE047: //home
+				mode == 0 ? inp_pos_cp = 0 : inp_pos_by = 0;
+				break;
+
+			case 0xE04F: //end
+				mode == 0 ? inp_pos_cp = 3 : inp_pos_by = 7;
+				break;
+
+			case 0x1B: goto bye; //esc
+		}
+
+	}
+bye:
+	printf("\nbye\n");
+
+	return 0;
+}
+
+
+void print_codepoint(char32_t currCodepoint) {
+
+	UTF_Point u8 = utf8_encode(currCodepoint),
+		u16be = utf16BE_encode(currCodepoint),
+		u16le = utf16LE_encode(currCodepoint),
+		u32be = utf32BE_encode(currCodepoint),
+		u32le = utf32LE_encode(currCodepoint);
+
+	if(currCodepoint < 32 || u8.result == UTF_ILLEGAL_CODEPOINT)
+		printf("\n [?] %s", u8.result == UTF_ILLEGAL_CODEPOINT ? "(illegal)" : "");
+	else
+		printf("\n [%.*s]", u8.num_bytes, u8.bytes);
+	clfw(30);
+	printf("\n\n");
+
+	auto print_point = [](const char *label, const UTF_Point *p) {
+		printf(" %s\n  ", label);
+		for(int i = 0; i < p->num_bytes; i++)
+			printf("%.2X ", static_cast<unsigned>(p->bytes[i]));
+		clfw(30);
+		printf("\n\n");
+	};
+
+	print_point("utf8", &u8);
+	print_point("utf16BE", &u16be);
+	print_point("utf16LE", &u16le);
+	print_point("utf32BE", &u32be);
+	print_point("utf32LE", &u32le);
+
+}
+
+void print_bytes() {
+	UTF_Point u8 = utf8_decode(bytes_buffer),
+		u16be = utf16BE_decode(bytes_buffer),
+		u16le = utf16LE_decode(bytes_buffer),
+		u32be = utf32BE_decode(bytes_buffer),
+		u32le = utf32LE_decode(bytes_buffer);
+
+	printf("\n\n\n");
+
+	auto print_point = [](const char *label, const UTF_Point *p) {
+		printf(" %s: %s", label, utf_result_str(p->result));
+		clfw(50);
+		printf("\n");
+		if(p->codepoint < 0x20 || p->result == UTF_ILLEGAL_CODEPOINT)
+			printf("  0x%.8X  [?]", p->codepoint);
+		else {
+			UTF_Point convu8 = *p;
+			if(p->type != UTF8)
+				convu8 = utf8_encode(p->codepoint);
+			printf("  0x%.8X  [%.*s]", p->codepoint, convu8.num_bytes, convu8.bytes);
+		}
+		clfw(10);
+		printf("\n\n");
+	};
+
+	print_point("utf8", &u8);
+	print_point("utf16BE", &u16be);
+	print_point("utf16LE", &u16le);
+	print_point("utf32BE", &u32be);
+	print_point("utf32LE", &u32le);
+
+}
---
+++ utf.hpp
@@ -0,0 +1,286 @@
+#pragma once
+#ifndef _UTF_H
+#define _UTF_H
+#include <stdint.h>
+
+#define UTF_API extern "C"
+
+enum UTF_TYPE {
+	UTF8,
+	UTF16,
+	UTF32,
+};
+
+enum UTF_BOM {
+	UTF_BE,
+	UTF_LE
+};
+
+enum UTF_RESULT {
+	UTF_OK,
+	UTF_UNEXPECTED_CONTINUATION,
+	UTF_TOO_FEW_WORDS,
+	UTF_OVERLONG,
+	UTF_ILLEGAL_CODEPOINT
+};
+
+
+struct UTF_Point {
+	char32_t codepoint = 0;
+	UTF_TYPE type{ UTF8 };
+	UTF_BOM bom{ UTF_LE };
+	UTF_RESULT result{ UTF_OK };
+	uint8_t num_words = 0, num_bytes = 0;
+	uint8_t bytes[8]{ 0 };
+};
+
+UTF_API UTF_Point utf8_decode(const void *stream_beg);
+
+UTF_API UTF_Point utf16_decode(const void *stream_beg, UTF_BOM en);
+UTF_API UTF_Point utf16BE_decode(const void *stream_beg);
+UTF_API UTF_Point utf16LE_decode(const void *stream_beg);
+
+UTF_API UTF_Point utf32_decode(const void *stream_beg, UTF_BOM en);
+UTF_API UTF_Point utf32LE_decode(const void *stream_beg);
+UTF_API UTF_Point utf32BE_decode(const void *stream_beg);
+
+
+UTF_API UTF_Point utf8_encode(char32_t codepoint);
+
+UTF_API UTF_Point utf16_encode(char32_t codepoint, UTF_BOM en);
+UTF_API UTF_Point utf16BE_encode(char32_t codepoint);
+UTF_API UTF_Point utf16LE_encode(char32_t codepoint);
+
+UTF_API UTF_Point utf32_encode(char32_t codepoint, UTF_BOM en);
+UTF_API UTF_Point utf32BE_encode(char32_t codepoint);
+UTF_API UTF_Point utf32LE_encode(char32_t codepoint);
+
+
+UTF_API inline UTF_RESULT utf_is_valid(char32_t codepoint) {
+	return (codepoint <= 0x10FFFF && ((codepoint & 0xFC00) != 0xD800)) ? UTF_OK : UTF_ILLEGAL_CODEPOINT;
+}
+
+
+
+
+#ifdef UTF_IMPLEMENTATION
+
+#include <memory>
+
+UTF_API UTF_Point utf8_decode(const void *s) {
+	const char *stream_beg = static_cast<const char *>(s);
+	UTF_Point ret;
+	auto set_bytes = [&](uint8_t num_words) {
+		ret.num_bytes = ret.num_words = num_words;
+		memcpy(ret.bytes, stream_beg, num_words);
+	};
+
+	switch(*stream_beg & 0xC0) {
+		case 0x00:
+		case 0x40:
+			// ascii
+			ret.codepoint = static_cast<char32_t>(*stream_beg);
+			goto return_one_word;
+		case 0x80:
+			ret.result = UTF_UNEXPECTED_CONTINUATION;
+		return_one_word:
+			set_bytes(1);
+			return ret;
+	}
+
+	uint8_t expWords = 2;
+	for(; expWords < 7 && (*stream_beg << expWords) & 0x80; expWords++);
+
+	size_t data = *stream_beg & (0xff >> (expWords + 1));
+
+	// TODO: detect overlong
+
+	for(uint8_t i = 1; i < expWords; i++) {
+		if(stream_beg[i] == 0 || (stream_beg[i] & 0xC0) != 0x80) {
+			ret.result = UTF_TOO_FEW_WORDS;
+			set_bytes(i);
+			return ret;
+		}
+		data <<= 6;
+		data |= stream_beg[i] & 0x3f;
+	}
+
+	ret.codepoint = static_cast<char32_t>(data);
+	ret.result = utf_is_valid(ret.codepoint);
+	
+	set_bytes(expWords);
+
+	return ret;
+}
+
+UTF_API UTF_Point utf16_decode(const void *s, UTF_BOM en) {
+	const uint8_t *stream_beg = static_cast<const uint8_t *>(s);
+	UTF_Point ret;
+	UTF_Point *debug_pointer_cuz_vs_sucks = &ret;
+	ret.type = UTF16;
+	ret.bom = en;
+
+	wchar_t W[2];
+	bool is_be = en == UTF_BE;
+	for(int i = 0; i < 2; i++) {
+		W[i] = (static_cast<wchar_t>(stream_beg[i * 2 + !is_be]) << 8) | static_cast<wchar_t>(stream_beg[i * 2 + is_be]);
+	}
+
+	switch(W[0] & 0xfc00) {
+		case 0xdc00: // low surrogate (invalid)
+			ret.result = UTF_UNEXPECTED_CONTINUATION;
+			ret.num_words = 1;
+			break;
+		case 0xd800: // high surrogate
+			if((W[1] & 0xfc00) != 0xdc00) {
+				ret.result = UTF_TOO_FEW_WORDS;
+				ret.num_words = 1;
+			} else {
+				ret.num_words = 2;
+				ret.codepoint = ((W[0] & 0x3ff) << 10) + (W[1] & 0x3ff) + 0x10000;
+			}
+			break;
+		default:
+			ret.codepoint = static_cast<char32_t>(W[0]);
+			ret.num_words = 1;
+	}
+
+	ret.num_bytes = 2 * ret.num_words;
+	memcpy(ret.bytes, stream_beg, ret.num_bytes);
+
+	return ret;
+}
+
+UTF_API UTF_Point utf16BE_decode(const void *s) {
+	return utf16_decode(s, UTF_BE);
+}
+
+UTF_API UTF_Point utf16LE_decode(const void *s) {
+	return utf16_decode(s, UTF_LE);
+}
+
+
+UTF_API UTF_Point utf32_decode(const void *s, UTF_BOM en) {
+	const uint8_t *stream_beg = static_cast<const uint8_t *>(s);
+	UTF_Point ret;
+	UTF_Point *debug_pointer_cuz_vs_sucks = &ret;
+	ret.type = UTF32;
+	ret.bom = en;
+	ret.num_words = 1;
+	ret.num_bytes = 4;
+	memcpy(ret.bytes, stream_beg, ret.num_bytes);
+	for(int i = 0; i < 4; i++)
+		ret.codepoint |= static_cast<char32_t>(stream_beg[en == UTF_BE ? 3 - i : i]) << i * 8;
+	ret.result = utf_is_valid(ret.codepoint);
+	return ret;
+}
+
+UTF_API UTF_Point utf32BE_decode(const void *s) {
+	return utf32_decode(s, UTF_BE);
+}
+
+UTF_API UTF_Point utf32LE_decode(const void *s) {
+	return utf32_decode(s, UTF_LE);
+}
+
+
+UTF_API UTF_Point utf8_encode(char32_t codepoint) {
+	UTF_Point ret;
+	ret.codepoint = codepoint;
+	ret.result = utf_is_valid(codepoint);
+
+	if(codepoint < 0x80) {
+		ret.num_bytes = ret.num_words = 1;
+		ret.bytes[0] = static_cast<char>(codepoint);
+		return ret;
+	}
+
+	uint8_t words[8]{ 0 };
+	uint8_t num_words = 0;
+	uint8_t word_val = 0;
+	char32_t header_bits = 0x1f;
+
+	for(;;) {
+		word_val = codepoint & 0x3f;
+		codepoint >>= 6;
+		words[7 - num_words] = word_val | 0x80;
+		num_words++;
+
+		if((codepoint & ~header_bits) == 0) {
+			word_val = static_cast<uint8_t>(0xff << (7 - num_words)) | (codepoint & header_bits);
+			words[7 - num_words] = word_val;
+			num_words++;
+			break;
+		}
+
+		header_bits >>= 1;
+	}
+
+	ret.num_bytes = ret.num_words = num_words;
+	memcpy(ret.bytes, words + 8 - ret.num_bytes, ret.num_bytes);
+
+	return ret;
+}
+
+UTF_API UTF_Point utf16_encode(char32_t codepoint, UTF_BOM en) {
+	UTF_Point ret;
+	UTF_Point *debug_pointer_cuz_vs_sucks = &ret;
+	ret.codepoint = codepoint;
+	ret.type = UTF16;
+	ret.bom = en;
+	ret.result = utf_is_valid(codepoint);
+
+	wchar_t W[2];
+	if(codepoint < 0x10000) {
+		ret.num_words = 1;
+		W[0] = codepoint & 0xffff;
+	} else {
+		ret.num_words = 2;
+		codepoint -= 0x10000;
+		W[0] = 0xD800 + (codepoint >> 10);
+		W[1] = 0xDC00 + (codepoint & 0x3ff);
+	}
+
+	ret.num_bytes = ret.num_words * 2;
+	for(int i = 0; i < 2; i++) {
+		uint8_t hibyte = W[i] >> 8, lobyte = W[i] & 0xff;
+		ret.bytes[i * 2]		= (en == UTF_BE) ? hibyte : lobyte;
+		ret.bytes[i * 2 + 1]	= (en == UTF_BE) ? lobyte : hibyte;
+	}
+	return ret;
+}
+
+UTF_API UTF_Point utf16BE_encode(char32_t codepoint) {
+	return utf16_encode(codepoint, UTF_BE);
+}
+
+UTF_API UTF_Point utf16LE_encode(char32_t codepoint) {
+	return utf16_encode(codepoint, UTF_LE);
+}
+
+
+UTF_API UTF_Point utf32_encode(char32_t codepoint, UTF_BOM en) {
+	UTF_Point ret;
+	UTF_Point *debug_pointer_cuz_vs_sucks = &ret;
+	ret.codepoint = codepoint;
+	ret.type = UTF32;
+	ret.bom = en;
+	ret.num_words = 1;
+	ret.num_bytes = 4;
+	ret.result = utf_is_valid(codepoint);
+	for(int i = 0; i < 4; i++)
+		ret.bytes[en == UTF_BE ? i : 3 - i] = static_cast<uint8_t>(codepoint >> i * 8);
+	return ret;
+}
+
+UTF_API UTF_Point utf32BE_encode(char32_t codepoint) {
+	return utf32_encode(codepoint, UTF_BE);
+}
+
+UTF_API UTF_Point utf32LE_encode(char32_t codepoint) {
+	return utf32_encode(codepoint, UTF_LE);
+}
+
+#endif // UTF_IMPLEMENTATION
+
+#endif // _UTF_H
